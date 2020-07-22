65edab32dbb056e8b2ca4a3fe9d55ffbb24771f0
Ivan <capital114@rambler.ru>
2020-07-22 16:19:54 +0300

    !

---
+++ src/Lyutyy/task6/Fish.java
@@ -0,0 +1,15 @@
+package Lyutyy.task6;
+
+public class Fish extends Animal {
+    public Fish() {
+    }
+
+    public Fish(String name, String color) {
+        super(name, color);
+    }
+
+    @Override
+    public void voice () {
+        System.out.println(super.getName() + " молчит");
+    }
+}
